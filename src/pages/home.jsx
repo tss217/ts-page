aea8d7ac5ae4bd4d589f693a552d81f863e06e14
ts217 <ts217@ts.ts>
2026-06-12 09:40:29 -0300

    api repo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,27 @@
 import "../App.css"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 import Menu from "../components/menu"
 
 export default function Home() {
-    return (
+    const [repos, setRepos] = useState(0)
 
+    useEffect(() => {
+        fetch("https://api.github.com/users/tss217")
+            .then(response => response.json())
+            .then(data => {
+                setRepos(data.public_repos)
+            })
+            .catch(error => {
+                console.error("Erro ao buscar repositórios:", error)
+            })
+    }, [])
+
+    return (
         <div className="main">
             <div className="containerOne">
 
-                <Menu/>
+                <Menu />
+
                 <main className="box">
                     <div className="titulo">
                         <h1>THIAGO</h1>
@@ -18,26 +31,23 @@ export default function Home() {
 
                     <div className="subtitulo">
                         <p>
-                            REPOSITORIOS NO <a href="https://github.com/tss217">GITHUB:60<br /></a>
-                            CONEXÕES NO LINDIN: 600
+                            REPOSITÓRIOS NO <a href="https://github.com/tss217">GITHUB: {repos}<br /></a>
+                            CONEXÕES NO LINKEDIN: 600
                         </p>
 
                         <p>
                             SKILLS: PYTHON, C++, REACT <br />
-                            BACKEND, SQL, LiNUX, API
+                            BACKEND, SQL, LINUX, API
                         </p>
 
                         <p>
-                       
                             ENTRE EM CONTATO<br />
-                            PELO <a href="https://www.linkedin.com/in/thiago-s-silva-ts217/">LINKDIN </a>
-                           
+                            PELO <a href="https://www.linkedin.com/in/thiago-s-silva-ts217/">LINKEDIN</a>
                         </p>
                     </div>
                 </main>
 
             </div>
         </div>
-        
     )
 }
